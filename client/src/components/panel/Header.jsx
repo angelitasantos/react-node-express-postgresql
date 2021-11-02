2993fc71c64f4dc83435c9207dbd6ef56f1b877b
Angelita Santos <angelitasantos@gmail.com>
2021-11-02 09:56:21 -0300

    configure navbar and create file style.css
    add style navbar
    
    Open #1

--- client/src/components/panel/Header.jsx
+++ client/src/components/panel/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './style.css';
 
 const Header = () => {
-    return (
-        <div>
-            <h1>Header Painel</h1>
-        </div>
-    )
-};
+  return (
+    <header className="topo">
+      <div className="topo-conteudo">
+        <Link className="topo-box" to="/">
+          <img className="topo-logo" src="./logo192.png" alt="logo app" title="logo app"/>
+          <p className="topo-nome-empresa">Project App</p>
+        </Link>
+
+        <nav className="topo-links">
+          <Link className="topo-link" to="/">Home</Link>
+          <Link className="topo-link" to="/painel">Painel</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
 
 export default Header;
