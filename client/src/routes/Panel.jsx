a87b536efd231488481f2e35c9a6490aa2bd1239
Angelita Santos <angelitasantos@gmail.com>
2021-11-01 21:09:08 -0300

    create file Panel.jsx inside routes folder

---
+++ client/src/routes/Panel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from "../components/panel/Header";
+import Sidebar from "../components/panel/Sidebar";
+import Footer from "../components/panel/Footer";
+
+const Painel = () => {
+    return (
+        <div>
+            <Header />
+            <Sidebar />
+            <Footer />
+        </div>
+    )
+};
+
+export default Painel;
