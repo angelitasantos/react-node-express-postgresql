d909d42707aeb7e961b22c76b3870ac908bbf436
Angelita Santos <angelitasantos@gmail.com>
2021-11-01 21:00:44 -0300

    create file Home.jsx inside routes folder

---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from "../components/home/Header";
+import Main from "../components/home/Main";
+import Footer from "../components/home/Footer";
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    )
+};
+
+export default Home;
